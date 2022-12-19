8a48c60797c8e00b5b091c042c430ceb52389299
braia <braia@DESKTOP-9F7M4NP>
2022-12-19 13:27:52 -0300

    Agregar archivos de proyecto.

---
+++ cliente.cs
@@ -0,0 +1,40 @@
+﻿/*Nombre completo
+
+Teléfono
+
+Dirección
+
+Email
+
+Si es nuevo cliente*/
+
+
+namespace cliente
+{
+    public struct Cliente
+    {
+        public string nombreCompleto { get; set; }
+        public string telefono { get; set; }
+        public string direccion { get; set; }
+        public string email { get; set; }
+        public bool nuevoCliente { get; set; }
+
+        public Cliente(string nombreCompleto, string telefono, string direccion, string email, bool nuevoCliente)
+        {
+            this.nombreCompleto = nombreCompleto;
+            this.telefono = telefono;
+            this.direccion = direccion;
+            this.email = email;
+            this.nuevoCliente = nuevoCliente;
+        }
+
+        public override string ToString() =>
+            "-------------------------------------------------" +
+            "\nDatos para el cliente " + nombreCompleto + ":" +
+            "\nTeléfono: " + telefono +
+            "\nDirección: " + direccion +
+            "\nEmail: " + email + 
+            "\nEs nuevo cliente?: " + ((nuevoCliente)? "Si" : "No") +
+            "\n-------------------------------------------------";
+    }
+}
---
+++ main.cs
@@ -0,0 +1,13 @@
+﻿using cliente;
+
+
+internal class main
+{
+    public static void Main(string[] args)
+    {
+        Cliente cliente = new Cliente(
+            "Pablo Gomez", "098534267", "Calle Durazno",
+            "pablo@mail.com", true);
+        Console.WriteLine(cliente); 
+    }
+}
